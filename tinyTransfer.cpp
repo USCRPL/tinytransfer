928d9e8217f09e4955eaa8e4563f6afd3ee5300e
Julian Maynes <thelastpocket@gmail.com>
2025-04-05 20:28:04 -0700

    Add partial comments

--- tinyTransfer.cpp
+++ tinyTransfer.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 
 #include "tinyTransfer.h"
-
+/**
+ * Fletcher16 checksum algorithm
+ * @param data Data to be made into a checksum
+ * @param length Length of input data
+ */
 uint16_t fletcher16(const uint8_t* data, uint64_t length){
     uint32_t c0, c1;
 
@@ -26,76 +30,121 @@ uint16_t fletcher16(const uint8_t* data, uint64_t length){
     }
     return (c1 << 8 | c0);
 }
-
+/** 
+ * Initializes a TinyTransferUpdatePacket object
+ * @param _data Payload to be put into a packet
+ * @param _length Length of input payload
+ * @param _packetId Unique id of packet
+ * @param _log Data log to be put into object log
+ * @param _logSize Size of input data log
+ * @param compressed Whether or not the data is to be compressed
+ * @param isIntegrator Whether the data is from the integrator
+ * @
+ */
 TinyTransferUpdatePacket::TinyTransferUpdatePacket(uint8_t* _data, uint16_t _length, uint32_t _packetId, char* _log, uint16_t _logSize, bool compressed, bool isIntegrator) : TinyTransferUpdatePacket() {
+    //Input data is compressed
     if (compressed) {
+        //Initialize compression
         heatshrink_encoder_reset(&hs_encoder);
-
         size_t count = 0;
         size_t sunk = 0;
         size_t polled = 0;
 
+        //Loop over all data
         while (sunk < _length) {
+            
+            //Feed data into encoder
             heatshrink_encoder_sink(&hs_encoder, &_data[sunk], _length - sunk, &count);
             sunk += count;
 
+            //If reached end of data, finish
             if (sunk >= _length) {
                 heatshrink_encoder_finish(&hs_encoder);
             }
-
+            
+            //Retrieve compressed data
             HSE_poll_res pres;
             do {
                 pres = heatshrink_encoder_poll(&hs_encoder, &payload[polled], sizeof(TinyTransferUpdatePacket) - polled, &count);
                 polled += count;
             } while (pres == HSER_POLL_MORE);
 
+            //If all data processed, finish process
             if (sunk == _length) {
                 heatshrink_encoder_finish(&hs_encoder);
             }
         }
-
+        //How much compressed data there was
         payloadSize = (uint16_t)polled;
+
+        //Set packet flag for compressed data
         packetFlags |= TINY_TRANSFER_UPDATE_FLAGS_COMPRESSED;
     }
     else {
+        //Copy data into the payload
         memcpy(payload, _data, _length);
+
+        //Length of payload is length of data
         payloadSize = _length;
+
+        //Set packet for uncompresse data
         packetFlags &= ~TINY_TRANSFER_UPDATE_FLAGS_COMPRESSED;
     }
 
     // Copy log (only for hamster packets)
     if (!isIntegrator) {
+        //Copy data into log
         memcpy(log, _log, _logSize);
         logSize = _logSize;
     } else {
         logSize = 0;
     }
 
+    //Set packet flag for integrator packet
     if (isIntegrator) {
         packetFlags |= TINY_TRANSFER_UPDATE_FLAGS_INTEGRATOR_PACK;
     }
     
+    //Update class with relevant input packet id and checksums
     packetId = _packetId;
-    
     payloadChecksum = fletcher16(payload, payloadSize);
     headerChecksum = fletcher16(header, sizeof(header));
 }
 
-uint16_t TinyTransferUpdatePacket::TinyTransferUpdatePacket::serialize(uint8_t* output) {
+/**
+ * Output object data into packet format
+ * @param output Input array to be filled with packet data
+ * @return Size of packet
+ */
+uint16_t TinyTransferUpdatePacket::serialize(uint8_t* output) {
+    //Header
     memcpy(output, header, sizeof(header));
+    //Header checksum
     memcpy(output + sizeof(header), &headerChecksum, sizeof(headerChecksum));
+    //Payload 
     memcpy(output + sizeof(header) + sizeof(headerChecksum), payload, payloadSize);
+    //Log
     memcpy(output + sizeof(header) + sizeof(headerChecksum) + payloadSize, log, logSize);
 
     return sizeof(header) + sizeof(headerChecksum) + payloadSize + logSize;
 }
 
+/**
+ * Check if packet is compressed
+ * @return Packet compressed
+ */
 bool TinyTransferUpdatePacket::isCompressed() {
     return packetFlags & TINY_TRANSFER_UPDATE_FLAGS_COMPRESSED;
 }
 
+/**
+ * Decompress given data into output
+ * @param output Input array to be given decompressed data
+ * @return Size of decompressed data
+ */
 uint16_t TinyTransferUpdatePacket::decompressPayload(uint8_t* output) {
     if (isCompressed()) {
+        //Decompression initialization
         size_t input_index = 0, output_index = 0;
         size_t input_size = 0, output_size = 0;
         HSD_poll_res poll_res;
@@ -103,10 +152,13 @@ uint16_t TinyTransferUpdatePacket::decompressPayload(uint8_t* output) {
 
         heatshrink_decoder_reset(&hs_decoder);
 
+        //Loop over all data
         while (input_index < payloadSize) {
+            //Decompress data
             heatshrink_decoder_sink(&hs_decoder, &payload[input_index], payloadSize - input_index, &input_size);
             input_index += input_size;
 
+            //Retrieve decompressed data
             do {
                 poll_res = heatshrink_decoder_poll(&hs_decoder, &output[output_index], sizeof(payload) - output_index, &output_size);
                 output_index += output_size;
@@ -116,6 +168,7 @@ uint16_t TinyTransferUpdatePacket::decompressPayload(uint8_t* output) {
 
         finish_res = heatshrink_decoder_finish(&hs_decoder);
 
+        //Final data decompressing?
         while (finish_res == HSDR_FINISH_MORE) {
             finish_res = heatshrink_decoder_finish(&hs_decoder);
             do {
@@ -127,36 +180,57 @@ uint16_t TinyTransferUpdatePacket::decompressPayload(uint8_t* output) {
         return (uint16_t)output_index;
     }
     else {
+        //If data isn't compressed spit back out raw payload
         memcpy(output, payload, payloadSize);
         return payloadSize;
     }
 }
 
-
+/**
+ * Constructor for update parser
+ */
 TinyTransferUpdateParser::TinyTransferUpdateParser() {
     init();
 }
 
+/**
+ * Initialize update parser object
+ */
 void TinyTransferUpdateParser::init(){
     completedPacket = inputPacket;
+    
+    //First state is looking for start of header 
     state = TINY_TRANSFER_PARSER_SEARCHING_FOR_SOH;
     soh = 0;
     inputPacket = TinyTransferUpdatePacket();
     position = 0;
 }
 
+/**
+ * Add a byte to update parser processing
+ * @param byte Input byte to be processed
+ * @return Whether or not a valid packet was found
+ */
 bool TinyTransferUpdateParser::processByte(uint8_t byte){
+    //Searching for MDLN
     if(state == TINY_TRANSFER_PARSER_SEARCHING_FOR_SOH){
+        //Read in another byte into soh
         soh = soh >> 8;
         soh |= (byte << 24);
 
+        //If MDLN found (little endian)
         if(soh == TINY_TRANSFER_UPDATE_SOH){
+
+            //Transition to next state
             state = TINY_TRANSFER_PARSER_HEADER;
+
+            //Put MDLN into input packet header, move onto next data
             memcpy(inputPacket.header, &soh, sizeof(soh));
             position = sizeof(soh);
         }
     }
 
+    //Search for rest of header
     else if (state == TINY_TRANSFER_PARSER_HEADER){
         inputPacket.header[position] = byte;
         position++;
@@ -167,44 +241,58 @@ bool TinyTransferUpdateParser::processByte(uint8_t byte){
         }
     }
 
+    //Validate header 
     else if (state == TINY_TRANSFER_PARSER_HEADER_CHECKSUM){
         ((uint8_t*)(&inputPacket.headerChecksum))[position] = byte;
         position++;
         if(position == sizeof(inputPacket.headerChecksum)){
             uint16_t redo_checksum = fletcher16(inputPacket.header, sizeof(TinyTransferUpdatePacket::header));
-
+            
+            //If checksum of header matches header checksum in the array
             if(redo_checksum == inputPacket.headerChecksum){
+                //Empty packet - still valid
                 if (inputPacket.payloadSize == 0) {
                     init();
                     return true;
                 }
 
+                //Transition to payload parsing
                 state = TINY_TRANSFER_PARSER_PAYLOAD;
                 position = 0;
             }
+
+            //invalid checksum, start over again
             else {
                 init();
             }
         }
     }
 
+    //
     else if (state == TINY_TRANSFER_PARSER_PAYLOAD) {
+        //Input payload into parser packet & move to the next byte
         inputPacket.payload[position] = byte;
         position++;
 
+        //If reached the end of payload - valid
         if(position >= inputPacket.payloadSize){
-            ///send packet or something or return true
             init();
             return true;
         }
-
     }
     return false;
 }
 
+/**
+ * Initialize RPC packet object
+ * @param _data
+ */
 TinyTransferRPCPacket::TinyTransferRPCPacket(uint8_t* _data) : TinyTransferRPCPacket() {
+    //Header
     memcpy(header, _data, sizeof(header));
+    //Header checksum
     memcpy(&headerChecksum, _data + sizeof(header), sizeof(headerChecksum));
+    //Arguments
     uint16_t copyLength = procArgsLength > TINY_TRANSFER_RPC_MAX_ARGS_SIZE ? TINY_TRANSFER_RPC_MAX_ARGS_SIZE : procArgsLength;
     memcpy(args, _data + sizeof(header) + sizeof(headerChecksum), copyLength);
 }
@@ -275,7 +363,7 @@ bool TinyTransferRPCParser::processByte(uint8_t byte){
     else if (state == TINY_TRANSFER_PARSER_PAYLOAD) {
         inputPacket.args[position] = byte;
         position++;
-        
+
         if(position >= inputPacket.procArgsLength){
             ///send packet or something or return true
             init();
